669e2a3f6de5de08dd754f643248d31e83fef1be
Riccardo Virgilio <riccardo.virgilio@gmail.com>
2019-08-05 15:21:19 +0200

    altre cosette

--- database/migrations/2019_08_03_141143_create_customers_table.php
+++ database/migrations/2019_08_03_141143_create_customers_table.php
@@ -18,6 +18,7 @@ class CreateCustomersTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('email');
+            $table->integer('active');
             $table->integer('type');
             $table->timestamps();
         });

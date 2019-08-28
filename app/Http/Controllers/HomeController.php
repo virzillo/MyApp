1d687aa0dd9855b5caf9067053b74dd45a280395
Riccardo Virgilio <riccardo.virgilio@gmail.com>
2019-08-28 19:01:26 +0200

    sist

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use App\Customer;
-=======
->>>>>>> 8b5dd385b05be11c1e6b35ef86177088149ae1d9
 use App\Company;
 
 class HomeController extends Controller
@@ -28,14 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
         $customers = Customer::all();
         $companies = Company::all();
 
         return view('home', compact('customers', 'companies'));
-=======
-        $companies = Company::all();
-        return view('home',compact('companies'));
->>>>>>> 8b5dd385b05be11c1e6b35ef86177088149ae1d9
     }
 }
